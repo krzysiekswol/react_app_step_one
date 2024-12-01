e991134cd09efe1475b5f517fdaf9bd881fbe89e
krzysztof_swol <krzysztof.swol@salesmanago.com>
2024-12-01 19:29:23 +0100

    add axios get fn

--- src/app/components/user-input/user-input.tsx
+++ src/app/components/user-input/user-input.tsx
@@ -1,14 +1,41 @@
+"use client";
+import { useState } from 'react';
 import styles from './user-input.module.css';
+import axios from 'axios';
 
 export default function UserInput() {
+  const [icao, setIcao] = useState<string>('');
+  const setUrl = (airportCode: string) => `https://avwx.rest/api/metar/${airportCode}?format=json&filter=sanitized&onfail=cache`
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIcao(event.target.value.toUpperCase());
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    _fetchMetar(icao);
+  }
+
+  const _fetchMetar = async (icao: string) => {
+    axios.get(setUrl(icao), {
+      headers: {
+        Authorization: 'Bearer ' + 'your api key'
+      }
+    }).then((response) => {
+      console.log(response.data);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
   return (
-    <form className="flex flex-wrap mt-8 justify-center">
+    <form className="flex flex-wrap mt-8 justify-center" onSubmit={handleSubmit}>
       <label className={styles.text}>
         ICAO Code:
-        <input type="text" className="mx-2 px-2 py-1 border border-gray-800" name="icao" />
+        <input type="text" value={icao} placeholder="Type ICAO code" onChange={handleChange} className="mx-2 px-2 py-1 border border-gray-800 text-gray-800" name="icao" />
       </label>
       <div className="w-full mt-6 flex justify-end">
-        <button className={styles.submit} type="submit">Submit</button>
+        <button className={styles.submit} type="submit" >Submit</button>
       </div>
     </form>
   )
